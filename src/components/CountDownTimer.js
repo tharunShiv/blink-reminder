6e8ed7c7306a434d217fb62a3a8403a59fab275a
Tharun Krishnamoorthy <tharun.moorthy@phonepe.com>
2021-07-03 23:55:22 +0530

    buttons, time work

--- src/components/CountDownTimer.js
+++ src/components/CountDownTimer.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { cloneElement, useContext } from "react";
 import "../styles/CountDownTimer.css";
+import UseSound from "use-sound";
+import notifyOne from "../assets/sounds/notify-1.mp3";
 
 const CountDownTimer = ({ hoursMinSecs }) => {
   const { hours = 0, minutes = 0, seconds = 60 } = hoursMinSecs;
+  const [play] = UseSound(notifyOne);
   const [[hrs, mins, secs], setTime] = React.useState([
     hours,
     minutes,
     seconds,
   ]);
+  const [currentMinutes, setCurrentInterval] = React.useState(60);
 
   const tick = () => {
     if (hrs === 0 && mins === 0 && secs === 0) reset();
@@ -20,13 +24,20 @@ const CountDownTimer = ({ hoursMinSecs }) => {
     }
   };
 
-  const reset = () =>
+  const reset = () => {
+    play();
     setTime([parseInt(hours), parseInt(minutes), parseInt(seconds)]);
+  };
 
   React.useEffect(() => {
     const timerId = setInterval(() => tick(), 1000);
     return () => clearInterval(timerId);
   });
+
+  if (currentMinutes != Number(minutes)) {
+    setCurrentInterval(minutes);
+    reset();
+  }
 
   return (
     <div className="timer-section">
--- src/components/IntervalSelector.js
+++ src/components/IntervalSelector.js
@@ -1,18 +1,91 @@
-import React from "react";
+import React, { useContext } from "react";
 import CountDownTimer from "./CountDownTimer.js";
 import "../styles/section.css";
 
 export default function Header() {
+  const regularTimer = { hours: 0, minutes: 60, seconds: 0 };
+  const shortInterval = { hours: 0, minutes: 30, seconds: 0 };
+  const longInterval = { hours: 0, minutes: 90, seconds: 0 };
+  const [interval, setInterval] = React.useState(regularTimer);
+  const [isRegularTimer, setIsRegularTimer] = React.useState(true);
+  const [isShortInterval, setIsShortInterval] = React.useState(false);
+  const [isLongInterval, setIsLongInterval] = React.useState(false);
+  const [shouldReset, setShouldReset] = React.useState(false);
+
+  const setNewInterval = (newInterval) => {
+    setInterval(newInterval);
+
+    setIsRegularTimer(false);
+    setIsShortInterval(false);
+    setIsLongInterval(false);
+
+    if (newInterval.minutes == 90) {
+      setIsLongInterval(true);
+    } else if (newInterval.minutes == 60) {
+      setIsRegularTimer(true);
+    } else {
+      setIsShortInterval(true);
+    }
+  };
+
   return (
     <>
       <div className="section">
         <div className="interval-selector-section">
-          <button className="regular-timer-button">regular timer</button>
-          <button className="short-interval-button">short intervals</button>
-          <button className="long-interval-button">long intervals</button>
+          {isRegularTimer ? (
+            <button
+              onClick={() => setNewInterval(regularTimer)}
+              className="regular-timer-button"
+            >
+              regular timer
+            </button>
+          ) : (
+            <button
+              onClick={() => setNewInterval(regularTimer)}
+              className="regular-timer-button-inactive"
+            >
+              regular timer
+            </button>
+          )}
+          {isShortInterval ? (
+            <button
+              onClick={() => setNewInterval(shortInterval)}
+              className="short-interval-button-active"
+            >
+              short intervals
+            </button>
+          ) : (
+            <button
+              onClick={() => setNewInterval(shortInterval)}
+              className="short-interval-button"
+            >
+              short intervals
+            </button>
+          )}
+          {isLongInterval ? (
+            <button
+              onClick={() => setNewInterval(longInterval)}
+              className="long-interval-button-active"
+            >
+              long intervals
+            </button>
+          ) : (
+            <button
+              onClick={() => setNewInterval(longInterval)}
+              className="long-interval-button"
+            >
+              long intervals
+            </button>
+          )}
         </div>
         <div className="timer">
-          <CountDownTimer hoursMinSecs={{ minutes: 90, seconds: 0 }} />
+          <CountDownTimer hoursMinSecs={{ ...interval }} reset={shouldReset} />
+        </div>
+        <div className="section-timer-actions">
+          {/* <button className="enable-notifications-button">
+            Enable notifications
+          </button> */}
+          <button className="reset-button">Reset</button>
         </div>
       </div>
     </>
